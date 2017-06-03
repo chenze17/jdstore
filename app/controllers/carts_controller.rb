7e8eb84f1115199dafff106edbb9591f39f954ac
chenze <czp__2013@163.com>
2017-06-03 12:52:49 +0800

    touch app/views/carts/checkout.html.erb

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,9 +7,5 @@ class CartsController < ApplicationController
     def checkout
       @order = Order.new
     end
-    private
-
-    def order_params
-      params.require(:order).permit(:billing_name, :billing_address, :shipping_name, :shipping_address)
-    end
+    
 end
