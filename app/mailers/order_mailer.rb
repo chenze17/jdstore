57bcd41310043a0ed48d7c713280c5c762f880ca
chenze <czp__2013@163.com>
2017-06-03 18:05:39 +0800

    add notify_cancel/ship function

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -25,7 +25,7 @@ class OrderMailer < ApplicationMailer
   def notify_cancel(order)
     @order        = order
     @user         = order.user
-    @product_lists = @order.product_lists
+    @product_lists = @order.product_lists 
 
     mail(to: @user.email, subject: "[JDStore] 您的订单 #{order.token}已取消")
   end
