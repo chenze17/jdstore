4418d2a9b97c56bfd574fc6f257b0d84baa0ce21
chenze <czp__2013@163.com>
2017-06-03 09:06:14 +0800

    only admin can access backend panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+u = User.new
+u.email = "czp__2013@163.com"           # 可以改成自己的 email
+
+u.password = "123456"                # 最少要六码
+
+u.password_confirmation = "123456"   # 最少要六码
+
+u.is_admin = true
+u.save
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
