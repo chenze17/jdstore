4d93aa4bdd21356749bc72021ad57909f335418e
chenze <czp__2013@163.com>
2017-06-03 20:59:08 +0800

    add apply_cancel function

--- config/routes.rb
+++ config/routes.rb
@@ -25,15 +25,16 @@ Rails.application.routes.draw do
     end
   end
   resources :cart_items
-  resources :orders
-  namespace :account do
-      resources :orders do
-        member do
-          post :pay_with_alipay
-          post :pay_with_wechat
-          post :apply_to_cancel
-        end
-      end
 
+
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+      post :apply_to_cancel
+    end
+  end
+  namespace :account do
+    resources :orders
   end
 end
