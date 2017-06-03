83885a3a6f7d1ff717783618f73934399b9bf894
chenze <czp__2013@163.com>
2017-06-03 16:47:19 +0800

    add admin/orders

--- app/helpers/admin/orders_helper.rb
+++ app/helpers/admin/orders_helper.rb
@@ -1,10 +1,2 @@
-class Admin::OrdersController < ApplicationController
-  layout "admin"
-
-  before_action :authenticate_user!
-  before_action :admin_required
-
-  def index
-    @orders = Order.order("id DESC")
-  end
+module Admin::OrdersHelper
 end
